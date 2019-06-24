fbf78a99376c03fcfffe1017c615a48f0e292c81
Subhransu <spanda@tavisca.in>
2019-06-24 09:04:33 +0530

    Problem 1 solution

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -16,14 +16,62 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         private static void Test(string args, int expected)
         {
+            /*var resutlts = FindDigit(args);
+            var result = resutlts.Equals(expected) ? "PASS" : "FAIL";*/
             var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"{args} : {result}");
+            //Console.WriteLine(resutlts);
+
         }
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            
+
+            String[] sp_eqn=equation.Split(new char[]{'*','='}); // Spitting the equation
+            if(sp_eqn[0].Contains("?"))
+            {
+                return multiplicant(sp_eqn[1],sp_eqn[2], sp_eqn[0]);
+            }
+            else if(sp_eqn[1].Contains("?"))
+            {
+                return multiplicant(sp_eqn[0],sp_eqn[2], sp_eqn[1]);
+            }
+            else 
+            {
+                
+                int res = Convert.ToInt32(sp_eqn[0])*Convert.ToInt32(sp_eqn[1]);
+                string r=Convert.ToString(res);
+                string c= sp_eqn[2];
+                int pos = c.IndexOf("?");
+                
+                if(c.Length==r.Length)
+                    return Convert.ToInt32(r[pos])-48;
+                return -1;
+
+
+            }
+        }
+        public static int multiplicant (string a, string b, string c )
+        {
+            int x= Convert.ToInt32(a);
+            int y= Convert.ToInt32(b);
+
+            if(y%x!=0)
+                return -1;
+            
+            int res = y/x;
+
+            string r=Convert.ToString(res);
+            int pos = c.IndexOf("?");
+            if (c.Length == r.Length)
+            {
+                return Convert.ToInt32(r[pos])-48;
+     
+
+            }
+            return -1;
+
         }
     }
 }
